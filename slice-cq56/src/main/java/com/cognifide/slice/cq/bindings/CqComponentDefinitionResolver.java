ab7dbf62ad0d0d62753ea5064eeb782f6ff1b8f1
Tomasz Rękawek <trekawek@gmail.com>
2015-01-07 15:04:54 +0100

    Use slice:model support from the Slice core.

--- slice-cq56/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
+++ slice-cq56/src/main/java/com/cognifide/slice/cq/bindings/CqComponentDefinitionResolver.java
@@ -22,41 +22,29 @@
 
 package com.cognifide.slice.cq.bindings;
 
-import javax.script.Bindings;
+import java.util.Map;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.scripting.api.BindingsValuesProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.cognifide.slice.api.model.ModelClassResolver;
+import com.cognifide.slice.api.provider.ComponentDefinitionResolver;
+import com.day.cq.wcm.api.components.ComponentManager;
 
 @Component
 @Service
-public class SliceBindingsProvider implements BindingsValuesProvider {
-
-	private static final Logger LOG = LoggerFactory.getLogger(SliceBindingsProvider.class);
+public class CqComponentDefinitionResolver implements ComponentDefinitionResolver {
 
 	@Reference
-	private ModelClassResolver modelClassNameResolver;
+	private ComponentManager componentManager;
 
 	@Override
-	public void addBindings(Bindings bindings) {
-		final Resource resource = (Resource) bindings.get("resource");
-		if (resource == null) {
-			return;
+	public Map<String, Object> getComponentDefinition(String resourceType) {
+		final com.day.cq.wcm.api.components.Component component = componentManager.getComponent(resourceType);
+		if (component != null) {
+			return component.getProperties();
 		}
-		try {
-			final Class<?> modelClass = modelClassNameResolver.getModelClass(resource.getResourceType());
-			if (modelClass != null) {
-				bindings.put("model", resource.adaptTo(modelClass));
-			}
-		} catch (ClassNotFoundException e) {
-			LOG.error("Can't resolver Slice model class", e);
-		}
+		return null;
 	}
 
 }
